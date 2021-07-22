8819a0fb94db123ad61aef4a7fffc777ef8d22e3
wang___nan <w791841277@163.com>
2021-07-22 17:57:44 +0800

    新建 Game, Team, Player库

--- sandbox_game/models.py
+++ sandbox_game/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    gameID = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.gameID
+
+
+class Team(models.Model):
+    teamName = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.teamName
+
+class Player(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
